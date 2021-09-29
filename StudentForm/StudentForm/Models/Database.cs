0a88806c4687503bd764e34506d03f54a4400875
Abu Jubaer <18-38784-3@student.aiub.edu>
2021-09-30 00:20:47 +0600

    student form

---
+++ StudentForm/StudentForm/Models/Database.cs
@@ -0,0 +1,24 @@
+﻿using StudentForm.Models.Tables;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace StudentForm.Models
+{
+    public class Database
+    {
+        public Students Students { get; set; }
+        //public Courses courses { get; set; }
+
+        public Database()
+        {
+            string connString = @"Server=DESKTOP-I5533B5\SQLEXPRESS;Database=UMS;Integrated Security=true";
+            SqlConnection conn = new SqlConnection(connString);
+            Students = new Students(conn);
+            //Courses = new Courses();
+
+        }
+    }
+}
