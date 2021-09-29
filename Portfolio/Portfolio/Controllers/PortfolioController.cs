bc119db5907789a625160ffef672286687ea0632
Abu Jubaer <18-38784-3@student.aiub.edu>
2021-09-29 14:13:02 +0600

    portfolio complete

---
+++ Portfolio/Portfolio/Controllers/PortfolioController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        // GET: Portfolio
+        public ActionResult Bio()
+        {
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            return View();
+        }
+
+        public ActionResult PersonalInfo()
+        {
+            return View();
+        }
+
+        public ActionResult Project()
+        {
+            return View();
+        }
+
+        public ActionResult Refference()
+        {
+            return View();
+        }
+    }
+}
